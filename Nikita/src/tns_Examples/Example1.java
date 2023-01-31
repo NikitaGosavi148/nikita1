360b1663aee9c5d633a9ba1b0bd590a4a913bfa0
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-01-31 14:12:52 +0530

    commit

---
+++ Nikita/src/tns_Examples/Example1.java
@@ -0,0 +1,18 @@
+package tns_Examples;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		char ch ='a';
+		char ch1=65;
+		char var='\u00A7';
+		char var2='\u20AC';
+		int a='A';
+		System.out.println(ch+"\\n"+ch1+"\n"+var+"\n"+var2+"\n"+a);
+		System.out.println("This is \"Nikki\"");
+		
+		
+
+	}
+
+}
---
+++ Nikita/src/tns_Examples/Example3.java
@@ -0,0 +1,13 @@
+package tns_Examples;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		int $number=20;
+		System.out.println("value of the number variable is:"+$number);
+		String studentName="Nikki";
+		System.out.println("value is:"+studentName);
+		System.out.println("\"\"");
+	}
+
+}
