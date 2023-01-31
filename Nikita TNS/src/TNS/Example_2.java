c04db2f5fb3cf3b079604d55ac8b3ed1dd62c90e
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-01-31 14:53:26 +0530

    commit

---
+++ Nikita TNS/src/TNS/Example_2.java
@@ -0,0 +1,14 @@
+package TNS;
+
+public class Example_2 {
+
+	public static void main(String[] args) {
+		int $number=20;
+		System.out.println("value of the number variable is:"+$number);
+		String studentName="Nikki";
+		System.out.println("value is:"+studentName);
+		System.out.println("\"\"");
+
+	}
+
+}
