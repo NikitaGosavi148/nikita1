7b4da3c5491107212d471c26efa90349bbb285a9
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-01 15:02:50 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day2/IfDemo.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int x=5;
+		if(x!=5)
+			System.out.println("vallue of x is not 5");
+		System.out.println("value of x :"+x);
+
+	}
+
+}
