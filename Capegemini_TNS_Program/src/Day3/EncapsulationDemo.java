b99038e113f69a3858a06e95b6fce556786b8973
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-02 16:24:53 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day3/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj=new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Nikita");
+		obj.setAge(21);
+		System.out.println(obj);
+
+	}
+
+}
