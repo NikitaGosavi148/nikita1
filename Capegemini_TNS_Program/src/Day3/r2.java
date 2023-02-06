9232baf289069d5c457e8363ab1195952959d71e
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-06 14:07:15 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day3/r2.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class r2 extends r1 {
+	public void j1(int o,string r){
+		System.out.println("You Can");
+	}
+
+	public static void main(String[] args) {
+		r1 g=newr1();
+		g.j1(76,"blue");
+	}
+
+}
