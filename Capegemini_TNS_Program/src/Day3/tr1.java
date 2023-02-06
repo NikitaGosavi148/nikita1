d0958e4c2085579dabca35ba34c0c2e9dbd8da4e
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-06 14:32:16 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day3/tr1.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class tr1 {
+	public void hu(int s,String q){
+		System.out.println("just");
+	}
+public void hu(String w,int d){
+	System.out.println("Kidding");
+}
+	public static void main(String[] args) {
+	tr1 dty=new tr1();
+	dty.hu(23, "ere");
+	dty.hu("ghdgx", 45);
+
+	}
+
+}
