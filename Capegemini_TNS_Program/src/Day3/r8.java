4d895471052d4a4f135ea73d54676879c1b5d7a7
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-06 15:03:39 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day3/r8.java
@@ -0,0 +1,25 @@
+package Day3;
+
+public class r8{
+		
+	
+public static class pt extends r2{
+		public void j(){
+		System.out.println("Taloda");
+		}
+		public static void main(String[] args) {
+		pt sd=new pt();
+		sd.j();
+		
+		sd.sr();
+
+		}
+		private void sr() {
+			// TODO Auto-generated method stub
+			System.out.println("Nasik");
+		}
+
+	}
+
+
+}
