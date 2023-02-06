5c04b026ce85d2536f00ff3cb12132ff982b7e39
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-06 13:58:08 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day4/se.java
@@ -0,0 +1,16 @@
+package Day4;
+
+public class se {
+	public void s1(int a,String b){
+	System.out.println("Overloading Method");
+	}
+	public void s1(String e,int g){
+	System.out.println("Method Overloading");
+	}
+	public static void main(String[] args){
+		se h=new se();
+		h.s1(12,"Swa");
+		h.s1("Swa",12);
+		
+	}
+}
