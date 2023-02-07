49f790dfcb791d72952ef89587158debb7728a85
Nikita Gosavi <Nikita Gosavi@LAPTOP-AJ8GIF6G>
2023-02-07 14:24:18 +0530

    commit

---
+++ Capegemini_TNS_Program/src/Day3/tr2.java
@@ -0,0 +1,12 @@
+package Day3;
+
+public class tr2 {
+
+	public static void main(String[] args) {
+		String s="Goverment";
+		System.out.println(s);
+		String v=s.replace('g', 'r');
+		System.out.println(v);
+	}
+
+}
